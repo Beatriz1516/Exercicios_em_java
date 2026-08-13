fa48f97b290dfb89b35c5ef38735676cec32edd9
Beatriz Vieira <gomes.pessoa@escola.pr.gov.br>
2026-08-13 11:51:34 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Olá, Mundo!");
-
         apresentacao(args);
         pergunta(args);
+
+        Scanner teclado = new Scanner(System.in);
+        String como_estou = teclado.nextLine();
+        System.out.println("Você está se sentindo " + como_estou);
+
+        e_bom(args);
+        String isso_e = teclado.nextLine();
+        System.out.println("Isso é " + isso_e);
+        teclado.close();
     }
 
     public static void apresentacao(String[] args){
@@ -16,4 +26,8 @@ public class Main {
     public static void pergunta(String[] args){
         System.out.println("Tudo bem com você?");
     } 
+
+    public static void e_bom(String[] args){
+        System.out.printf("Isso é bom ou ruim? ");
+    }
 }
